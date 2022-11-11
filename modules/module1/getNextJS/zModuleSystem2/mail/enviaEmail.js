0162ca87e55657e2143c37c93202c0549fd2f850
mayra <mayraluciaz12@gmail.com>
2022-11-11 10:56:17 -0300

    hotfix: alteração de email

--- modules/module1/getNextJS/zModuleSystem2/mail/enviaEmail.js
+++ modules/module1/getNextJS/zModuleSystem2/mail/enviaEmail.js
@@ -1,28 +1,28 @@
 function mail() {
-    let emailSender = false;
-    let message = "";
+  let emailSender = false;
+  let message = "";
 
-    if(user.getPermission != colaboratorPermConfig) {
-        stop();
-    }
-};
+  if (user.getPermission != colaboratorPermConfig) {
+    stop();
+  }
+}
 
-function send(to, from, message){
-    mail = getConection(url);
-    
-    if(!message) {
-        message = "Mensagem padrão";
-    }
-    
-    mail.send(to, from, message)
+function send(to, from, message) {
+  mail = getConection(url);
+
+  if (!message) {
+    message = "Mensagem padrão";
+  }
+
+  mail.send(to, from, message);
 }
 
 function conectaServidor() {
-    serverIp = "187.489.125.94";
-    serverName = "ifc-joinville";
-    
-    connection['ip'] = serverIp;
-    connection['server'] = serverName;
+  serverIp = "187.489.125.94";
+  serverName = "ifc-Araquari";
 
-    return connection.status;
+  connection["ip"] = serverIp;
+  connection["server"] = serverName;
+
+  return connection.status;
 }
